676ec23b3ed357df7703726faacb008dfb6df147
Jumshim <munjeffrey2003@gmail.com>
2020-09-19 19:12:26 -0700

    create base html

--- backend/static/script.js
+++ backend/static/script.js
@@ -5,18 +5,40 @@ $(document).ready(function() {
             title: $("#title").val(),
             description: $("#description").val()
         };
-
-        $.ajax({
-            url: url,
-            type: "POST",
-            data: JSON.stringify(postInfo),
-            processData: false,
-            contentType: "application/json; charset=UTF-8",
-            complete: function() {
-                console.log("request complete!");
-                window.location.reload();
-            }
-        });
-
     });
+    $.ajax({
+        url: url,
+        type: "POST",
+        data: JSON.stringify(postInfo),
+        processData: false,
+        contentType: "application/json; charset=UTF-8",
+        complete: function() {
+            console.log("request complete!");
+            window.location.reload();
+        }
+    });
+
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
+   
+    $('#sidebarCollapse').on('click', function () {
+        // open or close navbar
+        $('#sidebar').toggleClass('active');
+        // close dropdowns
+        $('.collapse.in').toggleClass('in');
+        // and also adjust aria-expanded attributes we use for the open/closed arrows
+        // in our CSS
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
+    function showPassword(){
+        var x = document.getElementById("password_input");
+            if (x.type === "password") {
+                x.type = "text";
+             } else {
+                x.type = "password";
+            }
+    }
 });
+
